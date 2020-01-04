0dc09743ef1cee70c699dda78d6867e39a1d4ba5
bartlomiejzachariasz <zachariasz@student.agh.edu.pl>
2020-01-04 14:27:17 +0100

    main

--- auto-docs-archiver-back/src/main.py
+++ auto-docs-archiver-back/src/main.py
@@ -1,4 +1,5 @@
 from src.api.app import app
+from src.resources.config import SERVER_CONFIG
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port="8000")
+    app.run(host=SERVER_CONFIG["host"], port=SERVER_CONFIG["port"])
